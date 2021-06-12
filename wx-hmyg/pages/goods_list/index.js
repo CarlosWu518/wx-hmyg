5f019948bda8cc143494f0a0e3721daada79d147
unknown <13776250336@163.com>
2021-06-12 10:55:07 +0800

    2021-6-12-1

--- wx-hmyg/pages/goods_list/index.js
+++ wx-hmyg/pages/goods_list/index.js
@@ -1,45 +1,62 @@
 // pages/goods_list/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    tabs:[
-     {
-        id:0,
-        value:"综合",
-        isActive:true
-     },
-     {
-      id:1,
-      value:"销量",
-      isActive:false
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        tabs: [{
+                id: 0,
+                value: "综合",
+                isActive: true
+            },
+            {
+                id: 1,
+                value: "销量",
+                isActive: false
+            },
+            {
+                id: 2,
+                value: "价格",
+                isActive: false
+            }
+        ],
+        goodList: []
     },
-    {
-      id:2,
-      value:"价格",
-      isActive:false
-      }
-    ]
-  },
+    //接口要的参数
+    QueryParams: {
+        query: "",
+        cid: "",
+        pagenum: 1,
+        pagesize: 10
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      console.log(options);
-  },
-  //标题点击事件
-  tabsItemChange(e){
-   //1.获取被点击的标题索引
-   const {index} = e.detail;
-   //2.修改原数组
-   let {tabs} = this.data;
-   tabs.forEach((v,i)=>i === index ? v.isActive=true : v.isActive=false);
-   //3.赋值到data中
-   this.setData({
-     tabs
-   })
-  }
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.QueryParams.cid = options.cid;
+        this.getGoodList();
+    },
+    //获取商品列表数据
+    async getGoodList() {
+        const res = await request({ url: "/goods/search", data: this.QueryParams });
+        this.setData({
+            goodList: res.goods
+        })
+    },
+    //标题点击事件
+    tabsItemChange(e) {
+        //1.获取被点击的标题索引
+        const { index } = e.detail;
+        //2.修改原数组
+        let { tabs } = this.data;
+        tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+        //3.赋值到data中
+        this.setData({
+            tabs
+        })
+    }
 })
