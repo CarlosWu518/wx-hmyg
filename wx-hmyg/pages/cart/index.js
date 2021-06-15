994f0441968c3b73bbf1ef443e3ee46c2138c787
CarlosWu518 <13776250336@163.com>
2021-06-15 18:23:58 +0800

    2021-6-15

--- wx-hmyg/pages/cart/index.js
+++ wx-hmyg/pages/cart/index.js
@@ -9,16 +9,16 @@ Page({
                 if (scopeAddress === true || scopeAddress === undefined) {
                     //获取收哦地址
                     wx.chooseAddress({
-                        success: (result1) => {
-                            console.log(result1);
+                        success: (address) => {
+                            wx.setStorageSync("address",address)
                         },
                     });
                 } else {
                     wx.openSetting({
                         success: (result2) => {
                             wx.chooseAddress({
-                                success: (result3) => {
-                                    console.log(result3);
+                                success: (address) => {
+                                    wx.setStorageSync("address",address)
                                 },
                             });
                         },
@@ -29,4 +29,21 @@ Page({
             complete: () => {}
         });
     }
+    //  // 点击 收货地址
+    //  async handleChooseAddress() {
+    //     //获取权限状态
+    //    const res1 = await getSetting();
+    //    const scopeAddress = res1.authSetting["scope.address"];
+    //     //判断权限状态
+    //     if (scopeAddress === true || scopeAddress=== undefined) {
+    //         //调用获取收货地址
+    //         const res2 = await chooseAddress();
+    //         console.log(res2);
+            
+    //     }else{
+    //         await openSetting();
+    //         const res2 = await chooseAddress();
+    //         console.log(res2);
+    //     }
+    // }
 })
