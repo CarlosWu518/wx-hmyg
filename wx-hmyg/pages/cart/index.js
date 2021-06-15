e3eaaaf4096f44cdf6cb4c5c04d794c060269006
unknown <13776250336@163.com>
2021-06-15 15:15:19 +0800

    2021-6-15-1

--- wx-hmyg/pages/cart/index.js
+++ wx-hmyg/pages/cart/index.js
@@ -1,66 +1,32 @@
 // pages/cart/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    // 点击 收货地址
+    handleChooseAddress() {
+        //获取权限状态
+        wx.getSetting({
+            success: (result) => {
+                const scopeAddress = result.authSetting["scope.address"];
+                if (scopeAddress === true || scopeAddress === undefined) {
+                    //获取收哦地址
+                    wx.chooseAddress({
+                        success: (result1) => {
+                            console.log(result1);
+                        },
+                    });
+                } else {
+                    wx.openSetting({
+                        success: (result2) => {
+                            wx.chooseAddress({
+                                success: (result3) => {
+                                    console.log(result3);
+                                },
+                            });
+                        },
+                    });
+                }
+            },
+            fail: () => {},
+            complete: () => {}
+        });
+    }
 })
